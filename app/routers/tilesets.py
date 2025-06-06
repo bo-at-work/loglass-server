cb5178975b21ac2910e0adb71f9026ea002587e8
Bo Lu <simplybolu@gmail.com>
2025-06-05 18:30:29 -0700

    Add docstring with real examples:

--- app/routers/tilesets.py
+++ app/routers/tilesets.py
@@ -28,6 +28,8 @@ async def list_tilesets(
 ):
     """
     Lists available tilesets using the stubbed repository.
+    i.e.
+        https://higlass.io/api/v1/tilesets/?limit=10000&dt=map-tiles&dt=matrix&dt=2d-projection&dt=arrowhead-domains&dt=2d-rectangle-domains&dt=2d-annotations&dt=bedpe&dt=chromsizes&dt=image-tiles&dt=scatter-point
     """
     tilesets, total_count = await repo.list_tilesets(
         autocomplete=ac, filetype=t, datatype=dt, order_by=o, reverse_order=r, page=page or 1, page_size=page_size or 10
@@ -58,6 +60,13 @@ async def get_tileset_info(
 ):
     """
     Retrieves detailed metadata for one or more tilesets using the stubbed repository.
+
+    GET request
+        https://higlass.io/api/v1/tileset_info/?d=IWgINpweRB2E0iKf4yMHMA&s=Z88rwOUCRgOq2GWdWJbszg
+
+    Response:
+    {"IWgINpweRB2E0iKf4yMHMA": {"min_pos": [0, 0], "max_pos": [3095693983, 3095693983], "max_zoom": 14, "max_width": 4194304000, "bins_per_dimension": 256, "transforms": [{"name": "ICE", "value": "weight"}], "chromsizes": [["chr1", 249250621], ["chr2", 243199373], ["chr3", 198022430], ["chr4", 191154276], ["chr5", 180915260], ["chr6", 171115067], ["chr7", 159138663], ["chr8", 146364022], ["chr9", 141213431], ["chr10", 135534747], ["chr11", 135006516], ["chr12", 133851895], ["chr13", 115169878], ["chr14", 107349540], ["chr15", 102531392], ["chr16", 90354753], ["chr17", 81195210], ["chr18", 78077248], ["chr19", 59128983], ["chr20", 63025520], ["chr21", 48129895], ["chr22", 51304566], ["chrX", 155270560], ["chrY", 59373566], ["chrM", 16571]], "name": "Wutz2017.HeLa.Scc1-AID_t180.HindIII.1000.mcool", "datatype": "matrix", "coordSystem": "", "coordSystem2": ""}}
+
     """
     infos = await repo.get_tileset_infos(d)
     return TilesetInfoResponse(data=infos)
@@ -70,6 +79,9 @@ async def get_tiles(
 ):
     """
     Fetches actual data tiles for one or more tilesets using the stubbed repository.
+    i.e.
+        https://higlass.io/api/v1/tiles/?d=OHJakQICQD6gTD7skx4EWA.4.10&s=Z88rwOUCRgOq2GWdWJbszg
+        https://higlass.io/api/v1/tiles/?d=OHJakQICQD6gTD7skx4EWA.2.0&d=OHJakQICQD6gTD7skx4EWA.2.1&s=Z88rwOUCRgOq2GWdWJbszg
     """
     tile_data = await repo.get_tiles_data(d)
     return TilesDataResponse(data=tile_data)
